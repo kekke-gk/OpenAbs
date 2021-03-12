9da616c6817c2d09021a1161a08e0e0c404765f1
kekke-gk <gk0509hn257@gmail.com>
2021-03-13 02:51:40 +0900

    Release v1.0

---
+++ background.js
@@ -0,0 +1,48 @@
+class arXiv {
+  static isMatch(url) {
+    const regex = /^https:\/\/arxiv\.org\/.*\.pdf$/gs;
+    return regex.exec(url) !== null
+  }
+
+  static conv2abs(url) {
+    const regex = /\d+\.\d+/gs;
+    const id = regex.exec(url)[0]
+    return `https://arxiv.org/abs/${id}`
+  }
+}
+
+class CVF {
+  static isMatch(url) {
+    const regex = /^https:\/\/openaccess\.thecvf\.com\/.*\.pdf$/gs;
+    return regex.exec(url) !== null
+  }
+
+  static conv2abs(url) {
+    const regex = /openaccess.thecvf.com\/(.*?)\/papers\/(.*?)\.pdf$/gs;
+    const ids = regex.exec(url).slice(1)
+    return `https://openaccess.thecvf.com/${ids[0]}/html/${ids[1]}.html`
+  }
+}
+
+const sites = [arXiv, CVF];
+
+function clickHandler(tab) {
+  const url = tab.url
+
+  console.log(url);
+
+  for (const site of sites) {
+    if (site.isMatch(url)) {
+      console.log("OpenAbs: match", site.name);
+
+      chrome.tabs.create({
+        "url": site.conv2abs(url),
+        "index": tab.index + 1
+      });
+
+      break;
+    }
+  }
+}
+
+chrome.action.onClicked.addListener(clickHandler);

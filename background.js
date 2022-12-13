cca382b4cccd7d7177cafad46793ecb8da39378b
kekke-gk <gk0509hn257@gmail.com>
2022-12-13 21:25:43 +0900

    Refactoring

--- background.js
+++ background.js
@@ -1,26 +1,26 @@
 class arXiv {
   static isMatch(url) {
     const regex = /^https:\/\/arxiv\.org\/.*\.pdf$/gs;
-    return regex.exec(url) !== null
+    return regex.exec(url) !== null;
   }
 
   static conv2abs(url) {
     const regex = /\d+\.\d+/gs;
-    const id = regex.exec(url)[0]
-    return `https://arxiv.org/abs/${id}`
+    const id = regex.exec(url)[0];
+    return `https://arxiv.org/abs/${id}`;
   }
 }
 
 class CVF {
   static isMatch(url) {
     const regex = /^https:\/\/openaccess\.thecvf\.com\/.*\.pdf$/gs;
-    return regex.exec(url) !== null
+    return regex.exec(url) !== null;
   }
 
   static conv2abs(url) {
     const regex = /openaccess.thecvf.com\/(.*?)\/papers\/(.*?)\.pdf$/gs;
-    const ids = regex.exec(url).slice(1)
-    return `https://openaccess.thecvf.com/${ids[0]}/html/${ids[1]}.html`
+    const ids = regex.exec(url).slice(1);
+    return `https://openaccess.thecvf.com/${ids[0]}/html/${ids[1]}.html`;
   }
 }
 
@@ -34,8 +34,8 @@ function openAbstractAsNewTab(url, tabIndex) {
       console.log("OpenAbs: match", site.name);
 
       chrome.tabs.create({
-        "url": site.conv2abs(url),
-        "index": tab.index + 1
+        url: site.conv2abs(url),
+        index: tabIndex + 1,
       });
 
       break;
@@ -50,25 +50,25 @@ function clickHandler(tab) {
 chrome.action.onClicked.addListener(clickHandler);
 
 function setIcon(url) {
-    for (const site of sites) {
-        if (site.isMatch(url)) {
+  for (const site of sites) {
+    if (site.isMatch(url)) {
       chrome.action.setIcon({ path: "images/icon128.png" });
-            return;
-        }
+      return;
     }
+  }
   chrome.action.setIcon({ path: "images/icon128_inactive.png" });
 }
 
-chrome.tabs.onActivated.addListener(activeInfo => {
-    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-        let url = tabs[0].url;
-        setIcon(url);
-    });
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+    let url = tabs[0].url;
+    setIcon(url);
+  });
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    const url = tab.url;
-    setIcon(url);
+  const url = tab.url;
+  setIcon(url);
 });
 
 function contextMenuClickHandler(info, tab) {
